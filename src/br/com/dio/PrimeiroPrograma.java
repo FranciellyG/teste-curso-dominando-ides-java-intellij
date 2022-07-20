b8461e166ce66fcbf0965243a6660f38ff3b902d
origin <https://github.com/FranciellyG/teste-curso-dominando-ides-java-intellij.git>
2022-07-19 21:57:29 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,6 +8,8 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro(nome:"As arveres somos nozes", numPaginas:300)
+        System.out.println(livro1);
 
 
 /*        int a = 5;
@@ -15,3 +17,39 @@ public class PrimeiroPrograma {
         System.out.println("Hello World! " + (a+b));*/
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
+
